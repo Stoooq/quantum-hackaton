587ab4515415493de8f8b805255843c061b39a59
Milosz Glowacki <milosz.glow@gmail.com>
2025-10-17 14:58:29 +0200

    quantum svc

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from qiskit_aer import AerSimulator
+from qiskit.circuit.library import ZZFeatureMap
+from qiskit_machine_learning.kernels import FidelityQuantumKernel
+
+df = pd.read_csv('alzheimers_disease_data.csv')
+y = df['Diagnosis']
+X = df.drop(['Diagnosis', 'DoctorInCharge'], axis=1)
+X = df[['Age', 'Gender', 'Ethnicity', 'EducationLevel', 'BMI']]
+X = X.head(100)
+y = y.head(100)
+
+def quantum_kernel_svc_pipeline(X, y,
+                                n_qubits=5,
+                                test_size=0.3,
+                                random_state=42,
+                                p_reps=1,
+                                entanglement='full',
+                                use_statevector=True):
+    Xnp = X.values
+    y = np.asarray(y)
+
+    X_reduced = Xnp.copy()
+    if X_reduced.shape[1] < n_qubits:
+        pad = np.zeros((X_reduced.shape[0], n_qubits - X_reduced.shape[1]))
+        X_reduced = np.hstack([X_reduced, pad])
+        print(f"Padding: rozszerzono cechy do wymiaru {n_qubits}")
+
+    scaler = MinMaxScaler(feature_range=(0, np.pi))
+    X_scaled = scaler.fit_transform(X_reduced)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=test_size, random_state=random_state, stratify=y if len(np.unique(y))>1 else None)
+    
+    feature_map = ZZFeatureMap(feature_dimension=n_qubits, reps=p_reps, entanglement=entanglement)
+    
+    quantum_kernel = FidelityQuantumKernel(feature_map=feature_map)
+    
+    print("przed")
+    K_train = quantum_kernel.evaluate(x_vec=X_train)
+    print("za")
+
+    K_test = quantum_kernel.evaluate(x_vec=X_test, y_vec=X_train)
+
+    svc = SVC(kernel='precomputed')
+    svc.fit(K_train, y_train)
+    y_pred = svc.predict(K_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+
+    print(acc)
+
+quantum_kernel_svc_pipeline(X, y)
+
